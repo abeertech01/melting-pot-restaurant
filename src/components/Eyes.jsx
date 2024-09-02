32dfe8c6a28c3c70de7f076758801dcea3c84fe4
abeertech01 <abeer.technology@gmail.com>
2024-09-03 01:07:16 +0600

    eyeballs' movements corrected

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -1,26 +1,36 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 
-// 1:49:18 / 3:01:14
 function Eyes() {
-  const [rotate, setRotate] = useState(0)
+  const [rotateLeftEye, setRotateLeftEye] = useState(0)
+  const [rotateRightEye, setRotateRightEye] = useState(0)
+  const leftEye = useRef(null)
+  const rightEye = useRef(null)
 
   useEffect(() => {
     window.addEventListener("mousemove", (e) => {
+      const leftEyeRect = leftEye.current.getBoundingClientRect()
+      const rightEyeRect = rightEye.current.getBoundingClientRect()
+
+      const leftEyeCenterX = leftEyeRect.left + leftEyeRect.width / 2
+      const leftEyeCenterY = leftEyeRect.top + leftEyeRect.height / 2
+      const rightEyeCenterX = rightEyeRect.left + rightEyeRect.width / 2
+      const rightEyeCenterY = rightEyeRect.top + rightEyeRect.height / 2
+
       let mouseX = e.clientX
       let mouseY = e.clientY
 
-      let deltaX = mouseX - window.innerWidth / 2
-      let deltaY = mouseY - window.innerHeight / 2
+      let deltaLeftEyeX = mouseX - leftEyeCenterX
+      let deltaLeftEyeY = mouseY - leftEyeCenterY
+      let deltaRightEyeX = mouseX - rightEyeCenterX
+      let deltaRightEyeY = mouseY - rightEyeCenterY
 
-      /**
-       * deltaX = center of width
-       * deltaY = center of height
-       * Math.atan2(y: number, x: number): number. Returns the angle (in radians) from the X axis to a point.
-       * (180 / Math.PI) = 1 radian
-       * with (180 / Math.PI), angle gets convert to degrees
-       */
-      var angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI)
-      setRotate(angle - 180)
+      var leftEyeAngle =
+        Math.atan2(deltaLeftEyeY, deltaLeftEyeX) * (180 / Math.PI)
+      var rightEyeAngle =
+        Math.atan2(deltaRightEyeY, deltaRightEyeX) * (180 / Math.PI)
+
+      setRotateLeftEye(leftEyeAngle - 180)
+      setRotateRightEye(rightEyeAngle - 180)
     })
   }, [])
 
@@ -33,11 +43,14 @@ function Eyes() {
       >
         <div className="absolute flex gap-10 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]">
           <div className="flex justify-center items-center w-[15vw] h-[15vw] rounded-full bg-zinc-100">
-            <div className="relative w-2/3 h-2/3 rounded-full bg-zinc-900">
+            <div
+              ref={leftEye}
+              className="relative w-2/3 h-2/3 rounded-full bg-zinc-900"
+            >
               {/* line  */}
               <div
                 style={{
-                  transform: `translate(-50%, -50%) rotate(${rotate}deg)`,
+                  transform: `translate(-50%, -50%) rotate(${rotateLeftEye}deg)`,
                 }}
                 className="line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-10"
               >
@@ -46,11 +59,14 @@ function Eyes() {
             </div>
           </div>
           <div className="flex justify-center items-center w-[15vw] h-[15vw] rounded-full bg-zinc-100">
-            <div className="relative flex justify-center items-center w-2/3 h-2/3 rounded-full bg-zinc-900">
+            <div
+              ref={rightEye}
+              className="relative flex justify-center items-center w-2/3 h-2/3 rounded-full bg-zinc-900"
+            >
               {/* line  */}
               <div
                 style={{
-                  transform: `translate(-50%, -50%) rotate(${rotate}deg)`,
+                  transform: `translate(-50%, -50%) rotate(${rotateRightEye}deg)`,
                 }}
                 className="line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-10"
               >
